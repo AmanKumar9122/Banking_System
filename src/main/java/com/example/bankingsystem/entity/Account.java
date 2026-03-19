3860487fe11267cf4e575337f9b1a4c1239a2003
AmanKumar9122 <amanraj15112003@gmail.com>
2026-03-19 20:13:49 +0530

    added logics in Account Service

--- src/main/java/com/example/bankingsystem/entity/Account.java
+++ src/main/java/com/example/bankingsystem/entity/Account.java
@@ -12,16 +12,23 @@ import java.util.List;
 
 @Entity
 public class Account {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     @Enumerated(EnumType.STRING)
     private AccountType type;
     private double balance;
+
+    private boolean isActive = true;
+
     @ManyToOne
     private Customer customer;
+
     @OneToMany(mappedBy = "account", cascade = CascadeType.ALL)
     private List<Loan> loans;
+
     @OneToMany(mappedBy = "account", cascade = CascadeType.ALL)
     private List<Transaction> transactions;
 }
--- src/main/java/com/example/bankingsystem/service/impl/AccountServiceImpl.java
+++ src/main/java/com/example/bankingsystem/service/impl/AccountServiceImpl.java
@@ -1,4 +1,57 @@
 package com.example.bankingsystem.service.impl;
 
-public class AccountServiceImpl {
+import com.example.bankingsystem.entity.Account;
+import com.example.bankingsystem.entity.Customer;
+import com.example.bankingsystem.entity.dto.AccountDto;
+import com.example.bankingsystem.exception.ResourceNotFoundException;
+import com.example.bankingsystem.repository.AccountRepository;
+import com.example.bankingsystem.repository.CustomerRepository;
+import com.example.bankingsystem.service.AccountService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class AccountServiceImpl implements AccountService {
+
+    private final AccountRepository accountRepository;
+    private final CustomerRepository customerRepository;
+    private final ModelMapper modelMapper;
+
+    @Override
+    public Account createAccount(Integer customerId, AccountDto dto) {
+        Customer customer = customerRepository.findById(customerId)
+                .orElseThrow(() -> new ResourceNotFoundException("Cannot create account: Customer not found with ID: " + customerId));
+
+        Account account = modelMapper.map(dto,Account.class);
+
+        account.setBalance(dto.getInitialDeposit());
+        account.setCustomer(customer);
+        account.setActive(true);
+        return accountRepository.save(account);
+    }
+
+    @Override
+    public Account getAccount(Integer id) {
+        return accountRepository.findById(id)
+                .orElseThrow(()-> new ResourceNotFoundException("Account not found with ID: " + id));
+    }
+
+    @Override
+    public List<Account> getAccountsByCustomer(Integer customerId) {
+        if (!customerRepository.existsById(customerId)) {
+            throw new ResourceNotFoundException("Customer not found with ID: " + customerId);
+        }
+        return accountRepository.findByCustomerId(customerId);
+    }
+
+    @Override
+    public void deactivateAccount(Integer id) {
+    Account account = getAccount(id);
+    account.setActive(false);
+    accountRepository.save(account);
+    }
 }
